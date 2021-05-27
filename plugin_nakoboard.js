170fe0b647b028c38a230f8636d0cb1c3e32c869
JunFritha <84742961+JunFritha@users.noreply.github.com>
2021-05-27 16:16:24 +0900

    Update plugin_nakoboard.js

--- plugin_nakoboard.js
+++ plugin_nakoboard.js
@@ -66,8 +66,8 @@ async function AD1input() {
 	await device.sendReport(outputReportId, outputReport);
 	
 	// recieve
-//	await WaitForInputReport()		// イベント発生まで待つ
-	await handleInputReport()		// イベント発生まで待つ
+	await WaitForInputReport()		// イベント発生まで待つ
+//	await handleInputReport()		// イベント発生まで待つ
 	console.log( `AD1input: ${ADval}` );
 // Add 2021/5/26 By Matsunaga /////////
 	ReadFlag = 1;
