30605fb838771ead7377dcc4d45305f36d94e150
oza-jv <ozakimak@gmail.com>
2021-09-26 18:16:37 +0900

    翻訳プログラムを修正

--- plugin_speak.js
+++ plugin_speak.js
@@ -1,5 +1,5 @@
 /**
- * なでしこ3 追加プラグイン 2021/3/15
+ * なでしこ3 追加プラグイン 2021/9/26
  * file : plugin_speak.js
  * サイト用に「声出」を追加するだけ。
  */
@@ -9,9 +9,21 @@ const PluginSpeak = {
     josi: [['と', 'を']],
     fn: function (text, sys) {
         speechSynthesis.cancel();
+
+        // 話者の特定
+        let voice = sys.__v0['話:話者'];
+        if (!voice) { voice = sys.__exec('話者設定', ['ja', sys]) }
+
         const uttr = new SpeechSynthesisUtterance(text);
+        
+        uttr.voice = voice;
+        if (voice) { uttr.lang = voice.lang }; // 必ず話者の特定に成功している訳ではない
+        uttr.rate = sys.__v0['話者速度']
+        uttr.pitch = sys.__v0['話者声高']
+        uttr.volume = sys.__v0['話者音量']
+        
         setTimeout(() => {
-        	speechSynthesis.speak(uttr);
+          speechSynthesis.speak(uttr);
         }, 500);
         //console.log("#声出: " + text);
     }
